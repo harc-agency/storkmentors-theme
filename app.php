a81ee8a4689135e60686643b0c05c6d7f0e8e506
Dustin Woodard <dustin.woodard@gmail.com>
2023-06-14 14:33:48 -0600

    fixes and plugins

--- app.php
+++ app.php
@@ -6,15 +6,14 @@
     <link rel='icon' href='favicon.ico' type='image/x-icon' />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist/app.css">
-
-    <?php echo wp_head(); ?>
-
+    <?php wp_head(); ?>
 </head>
 
 <body>
-    <?php echo bb_inject_inertia(); ?>
 
-    <?php echo wp_footer(); ?>
+    <?php bb_inject_inertia(); ?>
+
+    <?php wp_footer(); ?>
 </body>
 
 </html>
