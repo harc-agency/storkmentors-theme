3aff62f0c439e63d2623276390a2e295e1a7df6e
Dustin Woodard <dustin.woodard@gmail.com>
2023-06-21 09:46:25 -0600

    Add woocommerce pages

---
+++ src/js/Pages/Woocommerce/Cart/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <h1>cart</h1>
+</div>
+</template>
+
+<script setup>
+import { defineProps } from "vue"
+const props = defineProps({})
+</script>
+
+<style scoped>
+</style>
---
+++ src/js/Pages/Woocommerce/Shop/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>
+    Shop
+  </h1>
+</div>
+</template>
+
+<script setup>
+import { defineProps } from "vue"
+const props = defineProps({})
+</script>
+
+<style scoped>
+</style>
