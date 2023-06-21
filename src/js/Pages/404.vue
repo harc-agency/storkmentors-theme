49cbe17936ef13264df4c65d1afb07c4206985c9
Dustin Woodard <dustin.woodard@gmail.com>
2023-06-21 08:34:41 -0600

    404

--- src/js/Pages/404.vue
+++ src/js/Pages/404.vue
@@ -1,6 +1,18 @@
 <template>
 <section>
-  <h1>{{ props.content }}</h1>
+  <div class="p-20">
+    <h1 class="text-4xl font-bold text-center">
+      {{ props.content }}
+    </h1>
+
+    <div class="text-center">
+      <a href="/">
+        <button class="bg-primary text-white px-4 py-2 rounded">
+          Go Home
+        </button>
+      </a>
+    </div>
+  </div>
 </section>
 </template>
 
