11ddfe413ededa07be8b038646c76f8257fbf45a
Dustin Woodard <dustin.woodard@gmail.com>
2023-06-23 08:41:49 -0600

    white space

--- src/js/Components/Slider.vue
+++ src/js/Components/Slider.vue
@@ -1,40 +1,40 @@
 <template>
 <Swiper
-    effect="cards"
-    :keyboard="{
-        enabled: true,
-    }"
-    :speed="props.speed"
-    :autoplay="{
-        delay: props.delay,
-        disableOnInteraction: false,
-    }"
-    :loop="true"
-    :pagination="{
-        clickable: true,
-    }"
-    :modules="modules"
-    @swiper="onSwiper">
-    <slot>
-        <SwiperSlide v-for="(slide, i) in props.slides" :key="i">
-            <div
-                class="grid grid-cols-1 md:grid-cols-2 gap-10 place-self-center bg-base-200 p-4 rounded-box shadow-xl m-10">
-                <img :src="slide.image.url" :alt="slide.image.alt" />
-                <div class="grid h-full place-items-center">
-                    <div class="top-[20px] grid h-full place-items-center">
-                        <h1 class="text-3xl font-bold text-center text-secondary">{{ slide.title }}</h1>
-                        <div v-html="slide.description"></div>
+  effect="cards"
+  :keyboard="{
+    enabled: true,
+  }"
+  :speed="props.speed"
+  :autoplay="{
+    delay: props.delay,
+    disableOnInteraction: false,
+  }"
+  :loop="true"
+  :pagination="{
+    clickable: true,
+  }"
+  :modules="modules"
+  @swiper="onSwiper">
+  <slot>
+    <SwiperSlide v-for="(slide, i) in props.slides" :key="i">
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 gap-10 place-self-center bg-base-200 p-4 rounded-box shadow-xl m-10">
+        <img :src="slide.image.url" :alt="slide.image.alt" />
+        <div class="grid h-full place-items-center">
+          <div class="top-[20px] grid h-full place-items-center">
+            <h1 class="text-3xl font-bold text-center text-secondary">{{ slide.title }}</h1>
+            <div v-html="slide.description"></div>
 
-                        <a
-                            href="#"
-                            class="btn btn-primary bg-transparent text-secondary font-semibold hover:text-white py-2 px-4 border hover:border-transparent rounded transition ease-in-out duration-150">
-                            {{ slide.button_text }}
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </SwiperSlide>
-    </slot>
+            <a
+              href="#"
+              class="btn btn-primary bg-transparent text-secondary font-semibold hover:text-white py-2 px-4 border hover:border-transparent rounded transition ease-in-out duration-150">
+              {{ slide.button_text }}
+            </a>
+          </div>
+        </div>
+      </div>
+    </SwiperSlide>
+  </slot>
 </Swiper>
 </template>
 
